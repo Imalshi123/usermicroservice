10c1bb0a0eed88efddfcb3143935ee6e214adf0f
imalshi@sits.lk <imalshi@sits.lk>
2022-05-16 22:32:34 +0530

    first commit

--- server.js
+++ server.js
@@ -112,6 +112,6 @@ app.use('/user', UseController());
 
 
 app.get('/', (req, res) => {
-    res.send('Server Running in kuberntes')
+    res.send('Server Running in kuberntes on Pawani')
     
   })
