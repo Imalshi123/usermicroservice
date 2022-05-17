8c1d8714bc5673380446e6fad726a07b6449e21b
imalshi@sits.lk <imalshi@sits.lk>
2022-05-17 21:28:25 +0530

    CI/CD Intergration

--- server.js
+++ server.js
@@ -112,6 +112,6 @@ app.use('/user', UseController());
 
 
 app.get('/', (req, res) => {
-    res.send('Server Running in kubertes on Paani')
+    res.send('Server Running in kubrtes on Paani')
     
   })
